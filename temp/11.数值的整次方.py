caa396eefb63f4964effb1a900602d27774b6b5b
caozhengwenjing <15934492677@163.com>
2019-07-01 16:04:22 +0800

    add

---
+++ temp/11.数值的整次方.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        ans=1
+        for i in range(0,abs(exponent)):
+            ans=ans*base
+        if exponent>0:
+            return ans
+        else:
+            return 1/ans
+
+if __name__=='__main__':
+    solution=Solution()
+    base=float(input())
+    exponent=int(input())
+    ans=solution.Power(base,exponent)
+    print(ans)
