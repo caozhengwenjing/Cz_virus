43cdaaec1e9edba27677b5e4e4bb19197a9a609e
caozhengwenjing <15934492677@163.com>
2019-07-23 13:41:34 +0800

    add

--- temp/28.二叉搜索树与双向列表.py
+++ temp/28.二叉搜索树与双向列表.py
@@ -4,11 +4,15 @@
 # **思路：**递归将根结点和左子树的最右节点和右子树的最左节点进行连接起来。
 
 # -*- coding:utf-8 -*-
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
+
 class Solution:
     def Convert(self, pRootOfTree):
         # write code here
@@ -17,30 +21,30 @@ class Solution:
         if pRootOfTree.left is None and pRootOfTree.right is None:
             return pRootOfTree
 
-        #处理左子树
+        # 处理左子树
         self.Convert(pRootOfTree.left)
-        left=pRootOfTree.left
+        left = pRootOfTree.left
 
         if left:
             while left.right:
-                left=left.right
-            pRootOfTree.left,left.right=left,pRootOfTree
+                left = left.right
+            pRootOfTree.left, left.right = left, pRootOfTree
 
-        #处理右子树
+        # 处理右子树
         self.Convert(pRootOfTree.right)
-        right=pRootOfTree.right
+        right = pRootOfTree.right
 
         if right:
             while right.left:
-                right=right.left
-            pRootOfTree.right,right.left=right,pRootOfTree
+                right = right.left
+            pRootOfTree.right, right.left = right, pRootOfTree
 
         while pRootOfTree.left:
-            pRootOfTree=pRootOfTree.left
+            pRootOfTree = pRootOfTree.left
         return pRootOfTree
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     A1 = TreeNode(7)
     A2 = TreeNode(5)
     A3 = TreeNode(15)
@@ -50,13 +54,13 @@ if __name__=='__main__':
     A7 = TreeNode(19)
     A8 = TreeNode(24)
 
-    A1.left=A2
-    A1.right=A3
-    A2.left=A4
-    A2.right=A5
-    A3.left=A6
-    A3.right=A7
-    A7.right=A8
+    A1.left = A2
+    A1.right = A3
+    A2.left = A4
+    A2.right = A5
+    A3.left = A6
+    A3.right = A7
+    A7.right = A8
 
-    solution=Solution()
+    solution = Solution()
     solution.Convert(A1)
