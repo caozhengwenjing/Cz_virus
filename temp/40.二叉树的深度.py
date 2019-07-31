db790c57fcd42fba37360599462c336dfcd85b73
caozhengwenjing <15934492677@163.com>
2019-07-31 08:40:52 +0800

    add

--- temp/40.二叉树的深度.py
+++ temp/40.二叉树的深度.py
@@ -7,17 +7,19 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
     def TreeDepth(self, pRoot):
         # write code here
         if pRoot is None:
             return 0
-        left=self.TreeDepth(pRoot.left)
-        right=self.TreeDepth(pRoot.right)
-        print(left,right)
-        return max(left,right)+1
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        print(left, right)
+        return max(left, right) + 1
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     A1 = TreeNode(1)
     A2 = TreeNode(2)
     A3 = TreeNode(3)
@@ -25,12 +27,12 @@ if __name__=='__main__':
     A5 = TreeNode(5)
     A6 = TreeNode(6)
 
-    A1.left=A2
-    A1.right=A3
-    A2.left=A4
-    A2.right=A5
-    A4.left=A6
+    A1.left = A2
+    A1.right = A3
+    A2.left = A4
+    A2.right = A5
+    A4.left = A6
 
-    solution=Solution()
-    ans=solution.TreeDepth(A1)
-    print('ans=',ans)
+    solution = Solution()
+    ans = solution.TreeDepth(A1)
+    print('ans=', ans)
