7cd8a9624fd63db531f612757e04460712b2931c
caozhengwenjing <15934492677@163.com>
2019-08-02 08:22:25 +0800

    add

--- temp/42.数组中只出现一次的数字.py
+++ temp/42.数组中只出现一次的数字.py
@@ -6,21 +6,21 @@ class Solution:
     # 返回[a,b] 其中ab是出现一次的两个数字
     def FindNumsAppearOnce(self, array):
         # write code here
-        arrayset=set(array)
-        dict={}
+        arrayset = set(array)
+        dict = {}
         for num in arrayset:
-            dict[num]=0
-        for i in range(0,len(array)):
-            dict[array[i]]=dict[array[i]]+1
-        ans=[]
+            dict[num] = 0
+        for i in range(0, len(array)):
+            dict[array[i]] = dict[array[i]] + 1
+        ans = []
         for num in arrayset:
-            if dict[num]==1:
+            if dict[num] == 1:
                 ans.append(num)
         return ans
 
 
-if __name__=='__main__':
-    array=[1,1,2,2,3,3,4,5,5,6,7,7,9]
-    solution=Solution()
-    ans=solution.FindNumsAppearOnce(array)
+if __name__ == '__main__':
+    array = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
+    solution = Solution()
+    ans = solution.FindNumsAppearOnce(array)
     print(ans)
