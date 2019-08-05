0749be3bc8fcdd096fd3087cccace3bdcf18e0f8
caozhengwenjing <15934492677@163.com>
2019-08-05 11:09:35 +0800

    add

--- temp/01.替换空格.py
+++ temp/01.替换空格.py
@@ -1,40 +1,14 @@
-# class Solution:
-#     # s 源字符串
-#     def replaceSpace(self, s):
-#         # write code here
-#         temp = s.replace(" ", "%20")
-#         return temp
-#
-#
-# if __name__ == '__main__':
-#     s = 'We Are Happy'
-#     solution = Solution()
-#     ans = solution.replaceSpace(s)
-#     print(ans)
-
-
 class Solution:
+    # s 源字符串
     def replaceSpace(self, s):
+        # write code here
         temp = s.replace(" ", "%20")
         return temp
 
 
 if __name__ == '__main__':
-    s = "we are bim"
+    s = 'We Are Happy'
     solution = Solution()
     ans = solution.replaceSpace(s)
     print(ans)
-
-
-class Solution:
-    def repalpPlcw(self, s):
-        temp = s.replace(" ", "%9")
-        return temp
-
-
-if __name__ == '__main__':
-    s = "project and prejenct"
-    an = Solution().repalpPlcw(s)
-    print(an)
-
 
