177329f8939f0148e667b09e03ddb037071953da
caozhengwenjing <15934492677@163.com>
2019-08-05 11:24:46 +0800

    add

--- temp/07.跳台阶.py
+++ temp/07.跳台阶.py
@@ -1,3 +1,7 @@
+# **题目：**一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+#
+# **题解：**ans[n]=ans[n-1]+ans[n-2]
+
 class Solution:
     def jumpFloor(self, number):
         # write code here
--- temp/11.数值的整次方.py
+++ temp/11.数值的整次方.py
@@ -1,3 +1,5 @@
+# **题目：**给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+
 # -*- coding:utf-8 -*-
 
 
