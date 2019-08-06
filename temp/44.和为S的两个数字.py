180737fbdb19253331c7c65cb648e522749d6cf3
caozhengwenjing <15934492677@163.com>
2019-08-06 08:19:20 +0800

    add

--- temp/44.和为S的两个数字.py
+++ temp/44.和为S的两个数字.py
@@ -8,30 +8,31 @@
 class Solution:
     def FindNumbersWithSum(self, array, tsum):
         # write code here
-        ans=[]
-        i,j,minres=0,len(array)-1,1000000
-        for i in range(0,len(array)-1):
-            j=len(array)-1
+        ans = []
+        i, j, minres = 0, len(array) - 1, 1000000
+        for i in range(0, len(array) - 1):
+            j = len(array) - 1
             while True:
                 tempsum = array[i] + array[j]
                 if tempsum == tsum:
-                    if array[i]*array[j]<minres:
-                        ans=[]
+                    if array[i] * array[j] < minres:
+                        ans = []
                         ans.append(array[i])
                         ans.append(array[j])
-                        minres=array[i]*array[j]
+                        minres = array[i] * array[j]
                     break
                 else:
                     j = j - 1
-                if tempsum<tsum:
+                if tempsum < tsum:
                     break
-                if j<=i:
+                if j <= i:
                     break
         return ans
 
-if __name__=='__main__':
-    array=[1,2,4,7,11,15]
-    tsum=15
-    solution=Solution()
-    ans=solution.FindNumbersWithSum(array,tsum)
+
+if __name__ == '__main__':
+    array = [1, 2, 4, 7, 11, 15]
+    tsum = 15
+    solution = Solution()
+    ans = solution.FindNumbersWithSum(array, tsum)
     print(ans)
