573c2e0aa636cc23d145460a045ec1e446219db4
caozhengwenjing <15934492677@163.com>
2019-08-08 14:42:42 +0800

    add

--- temp/46.反转单词顺序.py
+++ temp/46.反转单词顺序.py
@@ -3,26 +3,29 @@
 # 后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a student.”。
 
 # -*- coding:utf-8 -*-
+
+
 class Solution:
     def ReverseSentence(self, s):
         # write code here
-        ans,word=[],''
-        for i in range(0,len(s)):
+        ans, word = [], ''
+        for i in range(0, len(s)):
             word = word + s[i]
-            if s[i]==' ':
+            if s[i] == ' ':
                 ans.append(word)
-                word=''
-            if i==len(s)-1:
-                word=word+' '
+                word = ''
+            if i == len(s) - 1:
+                word = word + ' '
                 ans.append(word)
         ans.reverse()
-        res=''
+        res = ''
         for c in ans:
-            res=res+c
-        return res[:len(res)-1]
+            res = res + c
+        return res[:len(res) - 1]
 
-if __name__=='__main__':
-    solution=Solution()
-    s='I am a student.'
-    ans=solution.ReverseSentence(s)
+
+if __name__ == '__main__':
+    solution = Solution()
+    s = 'I am a student.'
+    ans = solution.ReverseSentence(s)
     print(ans)
