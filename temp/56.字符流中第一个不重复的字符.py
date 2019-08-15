5e9b44ae20107e8acb436b912d17cd26cdfbbc1a
caozhengwenjing <15934492677@163.com>
2019-08-15 08:20:20 +0800

    add

--- temp/56.字符流中第一个不重复的字符.py
+++ temp/56.字符流中第一个不重复的字符.py
@@ -7,14 +7,15 @@
 class Solution:
     # 返回对应char
     def __init__(self):
-        self.all={}
-        self.ch=[]
+        self.all = {}
+        self.ch = []
+
     def FirstAppearingOnce(self):
         # write code here
         if self.all is None:
             return '#'
         for c in self.ch:
-            if self.all[c]==1:
+            if self.all[c] == 1:
                 return c
         return '#'
 
@@ -22,12 +23,13 @@ class Solution:
         # write code here
         self.ch.append(char)
         if char in self.all:
-            self.all[char]=self.all[char]+1
+            self.all[char] = self.all[char] + 1
         else:
-            self.all[char]=1
+            self.all[char] = 1
 
-if __name__=='__main__':
-    solution=Solution()
+
+if __name__ == '__main__':
+    solution = Solution()
     solution.Insert('g')
     ans = solution.FirstAppearingOnce()
     print(ans)
