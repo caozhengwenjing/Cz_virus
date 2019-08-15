441c0f81b4bb622b62d22b31652fe80e4ad8dde4
caozhengwenjing <15934492677@163.com>
2019-08-15 08:14:51 +0800

    add

--- temp/56.字符流中第一个不重复的字符.py
+++ temp/56.字符流中第一个不重复的字符.py
@@ -3,3 +3,46 @@
 #             当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
 #
 # **输出描述：**如果当前字符流没有存在出现一次的字符，返回#字符。
+# -*- coding:utf-8 -*-
+class Solution:
+    # 返回对应char
+    def __init__(self):
+        self.all={}
+        self.ch=[]
+    def FirstAppearingOnce(self):
+        # write code here
+        if self.all is None:
+            return '#'
+        for c in self.ch:
+            if self.all[c]==1:
+                return c
+        return '#'
+
+    def Insert(self, char):
+        # write code here
+        self.ch.append(char)
+        if char in self.all:
+            self.all[char]=self.all[char]+1
+        else:
+            self.all[char]=1
+
+if __name__=='__main__':
+    solution=Solution()
+    solution.Insert('g')
+    ans = solution.FirstAppearingOnce()
+    print(ans)
+    solution.Insert('o')
+    ans = solution.FirstAppearingOnce()
+    print(ans)
+    solution.Insert('o')
+    ans = solution.FirstAppearingOnce()
+    print(ans)
+    solution.Insert('g')
+    ans = solution.FirstAppearingOnce()
+    print(ans)
+    solution.Insert('l')
+    ans = solution.FirstAppearingOnce()
+    print(ans)
+    solution.Insert('e')
+    ans = solution.FirstAppearingOnce()
+    print(ans)
