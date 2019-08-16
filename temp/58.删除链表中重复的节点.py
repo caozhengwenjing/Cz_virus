3a1f14d0f8dfcf39d9f6db84558a49f4a2088a13
caozhengwenjing <15934492677@163.com>
2019-08-16 08:11:36 +0800

    add

--- temp/58.删除链表中重复的节点.py
+++ temp/58.删除链表中重复的节点.py
@@ -8,37 +8,40 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
 class Solution:
     def deleteDuplication(self, pHead):
         # write code here
-        num=[]
-        tempnum1=pHead
+        num = []
+        tempnum1 = pHead
         while tempnum1:
             num.append(tempnum1.val)
-            tempnum1=tempnum1.next
-        dict={}
+            tempnum1 = tempnum1.next
+        dict = {}
         for c in num:
-            dict[c]=0
+            dict[c] = 0
         for c in num:
-            dict[c]=dict[c]+1
-        newnum=[]
+            dict[c] = dict[c] + 1
+        newnum = []
         for c in num:
-            if dict[c]==1:
+            if dict[c] == 1:
                 newnum.append(c)
-        if newnum==[]:
+        if newnum == []:
             return None
-        head=ListNode(newnum[0])
-        temphead=head
-        for i in range(1,len(newnum)):
-            tempnode=ListNode(newnum[i])
-            temphead.next=tempnode
-            temphead=tempnode
+        head = ListNode(newnum[0])
+        temphead = head
+        for i in range(1, len(newnum)):
+            tempnode = ListNode(newnum[i])
+            temphead.next = tempnode
+            temphead = tempnode
         # while head:
         #     print(head.val)
         #     head=head.next
         return head
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     pHead1 = ListNode(1)
     pHead2 = ListNode(1)
     pHead3 = ListNode(1)
@@ -47,13 +50,13 @@ if __name__=='__main__':
     pHead6 = ListNode(1)
     pHead7 = ListNode(1)
 
-    pHead1.next=pHead2
-    pHead2.next=pHead3
-    pHead3.next=pHead4
-    pHead4.next=pHead5
-    pHead5.next=pHead6
-    pHead6.next=pHead7
+    pHead1.next = pHead2
+    pHead2.next = pHead3
+    pHead3.next = pHead4
+    pHead4.next = pHead5
+    pHead5.next = pHead6
+    pHead6.next = pHead7
 
-    solution=Solution()
-    ans=solution.deleteDuplication(pHead1)
+    solution = Solution()
+    ans = solution.deleteDuplication(pHead1)
     print(ans)
