27f2a2d3e5938fd08416d325156bd8aaca30bc8e
caozhengwenjing <15934492677@163.com>
2019-08-16 08:16:50 +0800

    add

--- temp/59.二叉树中的下一个节点.py
+++ temp/59.二叉树中的下一个节点.py
@@ -7,28 +7,33 @@
 #         3.节点不是根节点。如果该节点是其父节点的左孩子，则返回父节点；否则继续向上遍历其父节点的父节点，重复之前的判断，返回结果。
 
 # -*- coding:utf-8 -*-
+
+
 class TreeLinkNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
         self.next = None
+
+
 class Solution:
     def GetNext(self, pNode):
         # write code here
         if not pNode:
             return pNode
         if pNode.right:
-            left1=pNode.right
+            left1 = pNode.right
             while left1.left:
-                   left1=left1.left
+                left1 = left1.left
             return left1
 
         while pNode.next:
-            tmp=pNode.next
-            if tmp.left==pNode:
+            tmp = pNode.next
+            if tmp.left == pNode:
                 return tmp
-            pNode=tmp
+            pNode = tmp
 
-if __name__=='__main__':
-    solution=Solution()
+
+if __name__ == '__main__':
+    solution = Solution()
