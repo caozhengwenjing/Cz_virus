b7f5caab1a2619a3f8e0927e527f53441f11ee3e
caozhengwenjing <15934492677@163.com>
2019-08-23 09:02:49 +0800

    add

--- temp/63.序列化二叉树.py
+++ temp/63.序列化二叉树.py
@@ -8,17 +8,20 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 import collections
+
+
 class Solution:
     def Serialize(self, root):
         # write code here
         if not root:
             return None
-        res=[]
-        self.pre(root,res)
+        res = []
+        self.pre(root, res)
         return res
 
-    def pre(self,root,res):
+    def pre(self, root, res):
         if not root:
             return
         res.append(root.val)
@@ -27,20 +30,21 @@ class Solution:
         else:
             res.append('#')
         if root.right:
-            self.pre(root.right,res)
+            self.pre(root.right, res)
         else:
             res.append('#')
+
     def Deserialize(self, s):
-        if s=='':
+        if s == '':
             return None
-        vals=[]
-        for i in range(0,len(s)):
+        vals = []
+        for i in range(0, len(s)):
             vals.append(s[i])
-        vals=collections.deque(vals)
-        ans=self.build(vals)
+        vals = collections.deque(vals)
+        ans = self.build(vals)
         return ans
 
-    def build(self,vals):
+    def build(self, vals):
         if vals:
             val = vals.popleft()
             if val == '#':
@@ -51,8 +55,9 @@ class Solution:
             return root
         return self.build(vals)
 
+
 # [1, ',', 2, ',', 4, ',', ',', ',', 5, ',', ',', ',', 3, ',', 6, ',', ',', ',', 7, ',', ',']
-if __name__=="__main__":
+if __name__ == "__main__":
     A1 = TreeNode(1)
     A2 = TreeNode(2)
     A3 = TreeNode(3)
@@ -61,16 +66,16 @@ if __name__=="__main__":
     A6 = TreeNode(6)
     A7 = TreeNode(7)
 
-    A1.left=A2
-    A1.right=A3
-    A2.left=A4
-    A2.right=A5
-    A3.left=A6
-    A3.right=A7
+    A1.left = A2
+    A1.right = A3
+    A2.left = A4
+    A2.right = A5
+    A3.left = A6
+    A3.right = A7
 
     solution = Solution()
-    ans=solution.Serialize(A1)
+    ans = solution.Serialize(A1)
     print(ans)
-    root=solution.Deserialize(ans)
-    res=solution.Serialize(root)
+    root = solution.Deserialize(ans)
+    res = solution.Serialize(root)
     print(res)
