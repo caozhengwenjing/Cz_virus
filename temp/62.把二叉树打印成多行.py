acd0fd2739a2823ef85e8a5c346ecef76816f17d
caozhengwenjing <15934492677@163.com>
2019-08-23 08:37:56 +0800

    add

--- temp/62.把二叉树打印成多行.py
+++ temp/62.把二叉树打印成多行.py
@@ -5,18 +5,20 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
+
 class Solution:
     # 返回二维列表[[1,2],[4,5]]
     def Print(self, pRoot):
         # write code here
-        root=pRoot
+        root = pRoot
         if not root:
             return []
-        level=[root]
-        result=[]
+        level = [root]
+        result = []
         while level:
-            curvalues=[]
-            nextlevel=[]
+            curvalues = []
+            nextlevel = []
             for i in level:
                 curvalues.append(i.val)
                 if i.left:
@@ -24,11 +26,12 @@ class Solution:
                 if i.right:
                     nextlevel.append(i.right)
             if curvalues:
-                    result.append(curvalues)
+                result.append(curvalues)
             level = nextlevel
         return result
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     A1 = TreeNode(1)
     A2 = TreeNode(2)
     A3 = TreeNode(3)
@@ -37,13 +40,13 @@ if __name__=='__main__':
     A6 = TreeNode(6)
     A7 = TreeNode(7)
 
-    A1.left=A2
-    A1.right=A3
-    A2.left=A4
-    A2.right=A5
-    A3.left=A6
-    A3.right=A7
+    A1.left = A2
+    A1.right = A3
+    A2.left = A4
+    A2.right = A5
+    A3.left = A6
+    A3.right = A7
 
     solution = Solution()
-    ans=solution.Print(A1)
+    ans = solution.Print(A1)
     print(ans)
