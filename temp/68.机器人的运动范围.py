1b5f3d57ed9e15f5797111ff6e336caa211930e8
caozhengwenjing <15934492677@163.com>
2019-08-29 08:22:18 +0800

    add

--- temp/68.机器人的运动范围.py
+++ temp/68.机器人的运动范围.py
@@ -14,16 +14,16 @@ class Solution:
         return self.moving(threshold, rows, cols, 0, 0)
 
     def moving(self, threshold, rows, cols, row, col):
-        rowans,colans=0,0
-        rowtemp,coltemp=row,col
-        while rowtemp>0:
-            rowans=rowans+rowtemp%10
-            rowtemp=rowtemp//10
-        while coltemp>0:
-            colans=colans+coltemp%10
-            coltemp=coltemp//10
+        rowans, colans = 0, 0
+        rowtemp, coltemp = row, col
+        while rowtemp > 0:
+            rowans = rowans + rowtemp % 10
+            rowtemp = rowtemp // 10
+        while coltemp > 0:
+            colans = colans + coltemp % 10
+            coltemp = coltemp // 10
 
-        if rowans+colans>threshold:
+        if rowans + colans > threshold:
             return 0
         if row >= rows or col >= cols or row < 0 or col < 0:
             return 0
@@ -31,15 +31,15 @@ class Solution:
             return 0
         self.vis[(row, col)] = 1
 
-        return 1 + self.moving(threshold, rows, cols, row - 1, col) +\
-               self.moving(threshold, rows, cols, row + 1,col) + \
-               self.moving(threshold, rows,cols, row,col - 1) + \
+        return 1 + self.moving(threshold, rows, cols, row - 1, col) + \
+               self.moving(threshold, rows, cols, row + 1, col) + \
+               self.moving(threshold, rows, cols, row, col - 1) + \
                self.moving(threshold, rows, cols, row, col + 1)
 
 
-if __name__=='__main__':
-    solution=Solution()
-    threshold=10
-    rows,cols=1,100
-    ans=solution.movingCount(threshold,rows,cols)
+if __name__ == '__main__':
+    solution = Solution()
+    threshold = 10
+    rows, cols = 1, 100
+    ans = solution.movingCount(threshold, rows, cols)
     print(ans)
