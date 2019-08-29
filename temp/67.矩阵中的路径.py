7a8e6c482537d7421f74a4b43eeb8e411c892634
caozhengwenjing <15934492677@163.com>
2019-08-29 08:19:16 +0800

    add

--- temp/67.矩阵中的路径.py
+++ temp/67.矩阵中的路径.py
@@ -6,3 +6,37 @@
 #       路径不能再次进入该格子。
 #
 # **思路：**当起点第一个字符相同时，开始进行递归搜索，设计搜索函数。
+# -*- coding:utf-8 -*-
+class Solution:
+    def hasPath(self, matrix, rows, cols, path):
+        # write code here
+        for i in range(0,rows):
+            for j in range(0,cols):
+                if matrix[i*rows+j]==path[0]:
+                    if self.find_path(list(matrix),rows,cols,path[1:],i,j):
+                        return True
+        return False
+
+    def find_path(self,matrix,rows,cols,path,i,j):
+        if not path:
+            return True
+        matrix[i*cols+j]=0
+        if j+1<cols and matrix[i*cols+j+1]==path[0]:
+            return self.find_path(matrix,rows,cols,path[1:],i,j+1)
+        elif j-1>=0 and matrix[i*cols+j-1]==path[0]:
+            return self.find_path(matrix, rows, cols, path[1:], i, j - 1)
+        elif i+1<rows and matrix[(i+1)*cols+j]==path[0]:
+            return self.find_path(matrix, rows, cols, path[1:], i+1, j)
+        elif i-1>=0 and matrix[(i-1)*cols+j]==path[0]:
+            return self.find_path(matrix, rows, cols, path[1:], i-1, j)
+        else:
+            return False
+
+if __name__=='__main__':
+    solution=Solution()
+    matrix='ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS'
+    rows=5
+    cols=8
+    path='SGGFIECVAASABCEHJIGQEMS'
+    ans=solution.hasPath(matrix,rows,cols,path)
+    print(ans)
