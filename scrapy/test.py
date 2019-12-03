cfc6c11976469ad87f034846ff9f370c99e67c17
caozhengwnejing <15934492677@163.com>
2019-12-03 10:05:46 +0800

    add

--- scrapy/test.py
+++ scrapy/test.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 # 导入需要读取的第一个Excel表格的路径
-data1 = xlrd.open_workbook(r"C:\Users\Administrator\Desktop\掌小门1\安徽人联计算机科技有限公司.xlsx")
+data1 = xlrd.open_workbook(r"C:\Users\Administrator\Desktop\test.xlsx")
 table = data1.sheets()[0]
 # 创建一个空列表，存储Excel的数据
 tables = []
@@ -29,7 +29,7 @@ def import_excel(excel):
         file_name = [name]
         # path = r'C:\Users\Administrator\Desktop\test\w'
 
-        #循环判断文件夹是否存在，不存在则自动创建
+        # 循环判断文件夹是否存在，不存在则自动创建
         for name1 in file_name:
             if not os.path.exists('C:/Users/Administrator/Desktop/test/%s' % (name1)):
                 os.mkdir(name1)
@@ -41,17 +41,16 @@ def import_excel(excel):
         url2 = hetong
         # 获取姓名
         r = requests.get(url)
-        with open("C:/Users/Administrator/Desktop/test/%s/%s.jpg" % (name,name), "wb") as code:
+        with open("C:/Users/Administrator/Desktop/test/%s/%s.jpg" % (name, name), "wb") as code:
             code.write(r.content)
         # 获取正面
         r1 = requests.get(url1)
-        with open("C:/Users/Administrator/Desktop/test/%s/%s.jpg" % (name,name+str(1)), "wb") as code:
+        with open("C:/Users/Administrator/Desktop/test/%s/%s.jpg" % (name, name + str(1)), "wb") as code:
             code.write(r1.content)
         # 获取反面
         r2 = requests.get(url2)
-        with open("C:/Users/Administrator/Desktop/test/%s/%s.pdf" % (name,name), "wb") as code:
+        with open("C:/Users/Administrator/Desktop/test/%s/%s.pdf" % (name, name), "wb") as code:
             code.write(r2.content)
-
 
 
 if __name__ == '__main__':
@@ -59,5 +58,3 @@ if __name__ == '__main__':
     import_excel(table)
     for i in tables:
         print(i)
-
-
