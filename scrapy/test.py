53125f153e234f24e7f05d53cf078b320c9a2748
caozhengwnejing <15934492677@163.com>
2020-01-06 08:54:58 +0800

    add

--- scrapy/test.py
+++ scrapy/test.py
@@ -12,6 +12,8 @@ table = data1.sheets()[0]
 tables = []
 
 
+
+
 def import_excel(excel):
     for rown in range(excel.nrows):
         array = {'name': '', 'id': '', 'gongsi': '', 'zheng': '', 'fan': '', 'hetong': ''}
