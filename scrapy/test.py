5ae7d959526c10b0ff50c57dffbe1c484ff0b02d
caozhengwenjing <15934492677@163.com>
2020-04-06 14:55:30 +0800

    add

--- scrapy/test.py
+++ scrapy/test.py
@@ -13,7 +13,6 @@ table = data1.sheets()[0]
 # 创建一个空列表，存储Excel的数据
 tables = []
 
-
 def import_excel(excel):
     for rown in range(excel.nrows):
         array = {'name': '', 'id': '', 'gongsi': '', 'zheng': '', 'fan': '', 'hetong': ''}
